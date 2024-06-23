1dc0b5a47ef358221aa3c44c7ffce4d448beaa67
nagi <nagi>
2024-06-23 13:18:00 +0100

    added yaml to spec out llm pipeline

---
+++ services/model-cpu/test_model_cpu/test_llm_pipeline_yaml.py
@@ -0,0 +1,11 @@
+import yaml
+from pathlib import Path
+
+
+def test_yaml_reads_ok():
+    filename = Path(__file__).parents[1] / "src" / "model_cpu" / "llm-pipeline.yaml"
+    with open(filename) as f:
+        pipeline_spec = yaml.safe_load(f)
+    # sanity check some keys in the yaml
+    assert set(pipeline_spec.keys()) == {"pipeline"}
+    assert set(pipeline_spec["pipeline"].keys()) == {"pipeline-type", "kwargs", "model", "tokenizer"}
